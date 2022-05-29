8e06724523dfece3e2ab78f23e0c77cfd864eb8f
211sjp <1550636041@qq.com>
2022-05-29 23:45:30 +0800

    wiki build

--- flask_app.py
+++ flask_app.py
@@ -17,5 +17,18 @@ def strings(file_name):
 def evolve(file_name):
     return send_file('./evolve/'+file_name)
 
+@app.route("/wiki.html")
+def wiki_index():
+    return send_file("wiki.html")
+
+@app.route("/lib/<file_name>")
+def lib(file_name):
+    return send_file('./lib/'+file_name)
+
+@app.route("/wiki/<file_name>")
+def wiki(file_name):
+    return send_file('./wiki/'+file_name)
+
+
 if __name__ == "__main__":
     app.run()
